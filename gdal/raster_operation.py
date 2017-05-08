186aa08c60dff71f954f913403635571e53ab357
ben7276 <btan@esri.com>
2017-05-08 17:40:57 +0800

    add examples

---
+++ gdal/raster_operation.py
@@ -0,0 +1,59 @@
+import numpy.core.multiarray
+from osgeo import gdal
+
+
+def get_pixel_pos(lon, lat, E, N,  wres, hres):
+    x = int(lon - E) / wres
+    y = int(lat - N) / hres
+    return int(x), int(y)
+
+# only for one band
+def extent_clip(src_path, dst_path, N, S, E, W):
+    ds = gdal.Open(src_path)
+    driver = ds.GetDriver()
+
+    count = ds.RasterCount
+    band = ds.GetRasterBand(1)
+    arr = band.ReadAsArray()
+    cols = ds.RasterXSize
+    rows = ds.RasterYSize
+    prj = ds.GetProjection()
+
+    geotransform = ds.GetGeoTransform()
+    originX = geotransform[0]
+    originY = geotransform[3]
+    pixelWidth = geotransform[1]
+    pixelHeight = geotransform[5]
+
+    ngt = (E, geotransform[1],geotransform[2],N,geotransform[4],geotransform[5])
+    ltx, lty = get_pixel_pos(E, N, originX, originY, pixelWidth, pixelHeight)
+    rbx, rby = get_pixel_pos(W, S, originX, originY, pixelWidth, pixelHeight)
+
+    xSize = int(rbx - ltx)
+    ySize = int(-(lty - rby))
+
+    out = driver.Create(dst_path, xSize, ySize, 1, gdal.GDT_Float32)
+    out.SetGeoTransform(ngt)
+    na = numpy.zeros((ySize, xSize))
+
+    for x in range(ltx, rbx):
+        nx = x - ltx
+        for y in range(lty, rby):
+            ny = y - lty
+            na[ny][nx] = arr[y][x]
+
+    outband = out.GetRasterBand(1)
+    outband.WriteArray(na, 0, 0)
+    out.SetProjection(prj)
+    outband.FlushCache()
+
+if __name__ == '__main__':
+    nSet = 10
+    sSet = -10
+    eSet = -10
+    wSet = 10
+
+    extent_clip('world-dem.tif', 'test-dem.tif', nSet, sSet, eSet, wSet)
+
+
+
